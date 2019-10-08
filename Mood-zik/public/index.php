b374eed973ee9303eb03bae63f77da03163af31e
sebastie.peiris@he-arc <sebastien.peiris@he-arc.ch>
2019-10-08 11:31:37 +0200

    test iframe spotify basique

--- Mood-zik/public/index.php
+++ Mood-zik/public/index.php
@@ -58,3 +58,6 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
+
+?>
+<iframe src="https://open.spotify.com/embed/track/2leIuE4fdcJoNMSKu5Yr7a" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>
