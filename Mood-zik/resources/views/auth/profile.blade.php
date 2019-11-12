77d9fcf8adaa77519813454b8537c1f595363b9f
Sébastien Peiris <sebastien.peiris@he-arc.ch>
2019-11-12 21:58:33 +0100

    add mail to profile blade

--- Mood-zik/resources/views/auth/profile.blade.php
+++ Mood-zik/resources/views/auth/profile.blade.php
@@ -5,6 +5,11 @@
     <div class="card">
         <h1>{{ Auth::user()->name }}</h1>
     </div>
+    <div class="card">
+        <p>
+          {{ Auth::user()->email }}
+        </p>
+    </div>
 
 </div>
 @endsection
