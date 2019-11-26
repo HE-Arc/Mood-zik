7d51f700fbe32efe5122d1b35c7392c77c011540
Peiris Sébastien <sebastie.peiris@he-arc.ch>
2019-11-26 20:44:54 +0100

    post - list musique

--- Mood-zik/app/Http/Controllers/PostController.php
+++ Mood-zik/app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
 use App\Post;
+use DB;
 
 class PostController extends Controller
 {
@@ -26,5 +27,47 @@ class PostController extends Controller
         $post->save();
 
         return redirect('/home');
+    }
+
+    function action(Request $request)
+    {
+       if($request->ajax())
+       {
+          $query = $request->get('query');
+          if($query !='')
+          {
+            $data = DB::table('musics')
+                    ->where('title', 'like', '%'.$query.'%')
+                    ->orWhere('Artist', 'like', '%'.$query.'%')
+                    ->orderBy('id', 'asc')
+                    ->get();
+          }
+          else {
+            $data = DB::table('musics')
+                    ->orderBy('id', 'asc')
+                    ->get();
+          }
+          $total_row = $data->count();
+          if($total_row > 0)
+            {
+               foreach($data as $row)
+               {
+                  $output .= '
+                  <tr>
+                   <td>'.$row->Title.'</td>
+                   <td>'.$row->Artist.'</td>
+                  </tr>
+                  ';
+               }
+             }
+            else
+              {
+                 $output = '
+                 <tr>
+                  <td align="center" colspan="5">No Data Found</td>
+                 </tr>
+                 ';
+              }
+       }
     }
 }
--- Mood-zik/resources/views/create_post.blade.php
+++ Mood-zik/resources/views/create_post.blade.php
@@ -2,6 +2,9 @@
 
 @section('head')
   <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 @endsection
 
 @section('content')
@@ -16,7 +19,22 @@
                 <div><label for="title">Entrez votre titre</label>
                 <div></div> <textarea class="input" id="title" name="title" cols="50" rows="1">Titre</textarea> </div>
                 <div> <label for="title">Choisissez votre morceau</label>
-                <div></div> <textarea class="input" id="embed" name="embed" cols="50" rows="1">Morceau</textarea> </div>
+                <div></div> <textarea class="input" id="search" name="search" placeholder="Search Music" cols="50" rows="1"></textarea></div>
+                <div class="table-responsive">
+                  <h3 align="center">Musiques : <span id="total_records"></span></h3>
+                  <table class="table table-striped table-bordered">
+                    <thead>
+                      <tr>
+                        <th>Titre</th>
+                        <th>Artiste</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                  </table>
+
+                </div>
                 <div> <label for="title">Entrez votre texte (max 200 caractères)</label>
                 <div></div> <textarea class="input" id="text" name="text" cols="50" rows="5" maxlength="200">Texte</textarea> </div>
 
@@ -35,4 +53,34 @@
     </div>
     <div><label for="title"><p>Commentaire: Commencer par implémenter la création d'un post en collant le code spotify d'un iframe</p></label></div>
 </form>
+@endsection
+
+@section('script')
+<script>
+  $('document').ready(function(){
+
+      fetch_music_data();
+
+      function fetch_music_data(query = '')
+      {
+          $.ajax({
+              url:"{{ route('live_search.action') }}",
+              method: 'GET',
+              data: {query:query},
+              dataType: 'json',
+              success: function(data)
+              {
+                  $('tbody').html(data.table_data);
+                  $('#total_records').text(data.total_data);
+              }
+          })
+      }
+
+      $(document).on('keyup', '#search', function(){
+        var query = $(this).val();
+        fetch_music_data(query);
+      });
+  });
+</script>
+
 @endsection
