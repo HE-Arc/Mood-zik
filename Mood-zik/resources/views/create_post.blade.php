6d882ca25ab31aa674004eca1f89469f79be7b61
jonas.vallat <jonas.vallat@he-arc.ch>
2019-12-10 10:33:09 +0100

    css changes to new post view

--- Mood-zik/resources/views/create_post.blade.php
+++ Mood-zik/resources/views/create_post.blade.php
@@ -15,18 +15,19 @@
     <div class=post>
         <div class="conteneur_v">
             <div class="post_content">
-                <div><label for="title"><h3>Nouveau post</h3></label></div>
-                <div><label for="title">Entrez votre titre</label>
-                <div></div> <textarea class="input" id="title" name="title" cols="50" rows="1">Titre</textarea> </div>
+                <div><label for="title"><h3>Nouveau post</h3></label>
+
+                <div class="autocomplete" style="300px;"> <label for="title">Entrez votre titre</label>
+                <input id="title" type="text" name="title" placeholder="Titre" size="35"/>
+                </div></div>
+
                 <div class="autocomplete" style="300px;"> <label for="title">Choisissez votre morceau</label>
-
-
-                <input id="music" type="text" name="music" placeholder="Music" />
-
+                <input id="music" type="text" name="music" placeholder="Music" size="35"/>
                 </div>
 
-                <div> <label for="title">Entrez votre texte (max 200 caractères)</label>
-                <div></div> <textarea class="input" id="text" name="text" cols="50" rows="5" maxlength="200">Texte</textarea> </div>
+                <div class="autocomplete" style="300px;"> <label for="title">Entrez votre texte (max 200 caractères)</label>
+                <input id="text" type="text" name="text" placeholder="Texte" size="35"/>
+                </div></div>
 
                 @if ($errors->any())
                     <div class="alert alert-danger">
@@ -37,7 +38,7 @@
                         </ul>
                     </div>
                 @endif
-                <button type="submit" class="btn btn-primary">Submit</button>
+                </div><button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
     </div>
