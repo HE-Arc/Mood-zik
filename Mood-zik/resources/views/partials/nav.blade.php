433f1061834bac7419dfb6e0abeaf7905377541d
robin.alfred <robin.alfred@he-arc.ch>
2019-12-10 14:50:48 +0100

    CSS Navigation

--- Mood-zik/resources/views/partials/nav.blade.php
+++ Mood-zik/resources/views/partials/nav.blade.php
@@ -1,32 +1,12 @@
 <nav>
-  <ul>
-    <li>
-      <a href="{{ url('/') }}">{{ config('app.name', 'Mood\'Zik') }}</a>
-    </li>
-    <li>
-      @guest
-          <h1>invité</h1>
-      @else
-          <a href="{{ route('post') }}">Nouveau Poste</a>
-      @endguest
-    </li>
-    <li>
-      <ul>
+      <a href="{{ url('/') }}">Accueil</a>
+      <a href="{{ route('post') }}">Ajouter un post</a>
         @guest
-          <li>
                 <a href="{{ route('login') }}">{{ __('Login') }}</a>
-          </li>
           @if (Route::has('register'))
-            <li>
               <a href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
           @endif
         @else
-          <li>
             <a href="{{ route('profile')}}">{{ Auth::user()->name }}</a>
-          </li>
         @endguest
-      </ul>
-    </li>
-  </ul>
 </nav>
