e8eeb416620542f29c3e53e404ba2a9154ae42cc
robin.alfred <robin.alfred@he-arc.ch>
2019-12-15 18:19:24 +0100

    Design

--- Mood-zik/resources/views/playlist.blade.php
+++ Mood-zik/resources/views/playlist.blade.php
@@ -4,10 +4,17 @@
 
 @endsection
 
+@if(Auth::check())
 @section('content')
 <div class="container">
   <h1>Ma playlist</h1>
 </div>
+@else
+
+  <h2>Veuillez vous concentrer un minimum et vous connecter pour bénificer de toutes les fonctionnalités</h1>
+
+@endif
+
 
 
 @endsection
