ebcda130a8190483503a17f3444329c033480c6a
robin.alfred <robin.alfred@he-arc.ch>
2019-12-15 10:36:12 +0100

    Test

--- Mood-zik/resources/views/auth/register.blade.php
+++ Mood-zik/resources/views/auth/register.blade.php
@@ -20,7 +20,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Votre nom: ') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Votdre nom: ') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
