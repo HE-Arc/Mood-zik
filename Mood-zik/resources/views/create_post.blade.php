2d5a0ac63093f997482ff6416e3db965a90fed63
robin.alfred <robin.alfred@he-arc.ch>
2019-12-15 12:52:07 +0100

    New posts

--- Mood-zik/resources/views/create_post.blade.php
+++ Mood-zik/resources/views/create_post.blade.php
@@ -9,33 +9,45 @@
 @endsection
 
 @section('content')
+
+@if(Auth::check())
 <section class="form_zone">
   <h1> Ajouter un nouveau post </h1>
-  <form action="{{ route('post') }}" method="post" autocomplete="off">
-    {{ csrf_field() }}
-    <section class="input_zone">
-            <label class="input">Titre du post</label>
-            <input type="text" name="title" required />
-    </section>
-
-    <section class="input_zone">
 
 
-      <div class="autocomplete" style="300px;">
-      <label for="title">Choisissez votre morceau</label>
-      <input id="music" type="text" name="music" placeholder="Music" />
-    </div>
-        </section>
 
-    <section class="input_zone">
-            <label>Votre description:</label>
-             <textarea  name="text"  rows="1" cols="33" maxlength="200"></textarea>
-    </section>
+<form action="{{ route('post') }}" method="post" autocomplete="off">
+      {{ csrf_field() }}
+      <section class="input_zone">
+              <label class="input">Titre du post</label>
+              <input type="text" name="title" required />
+      </section>
+
+      <section class="input_zone">
 
 
-    <button type="submit" class="btn btn-primary">Ajouter</button>
-  </form>
+        <div class="autocomplete" style="300px;">
+        <label for="title">Choisissez votre morceau</label>
+        <input id="music" type="text" name="music" placeholder="Music" />
+      </div>
+          </section>
+
+      <section class="input_zone">
+              <label>Votre description:</label>
+               <textarea  name="text"  rows="1" cols="33" maxlength="200"></textarea>
+      </section>
+
+
+      <button type="submit" class="btn btn-primary">Ajouter</button>
+    </form>
+
+
 </section>
+@else
+
+  <h2>Veuillez vous concentrer un minimum et vous connecter pour bénificer de toutes les fonctionnalités</h1>
+
+@endif
 
 
 
