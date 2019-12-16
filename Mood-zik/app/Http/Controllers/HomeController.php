5748d7c1563a3c1bc000cae5104472637f0a6d1d
sebastie.peiris@he-arc <sebastien.peiris@he-arc.ch>
2019-12-17 00:09:34 +0100

    correction bugs

--- Mood-zik/app/Http/Controllers/HomeController.php
+++ Mood-zik/app/Http/Controllers/HomeController.php
@@ -23,15 +23,16 @@ class HomeController extends Controller
 
     /**
      * Returns the home blade with an array of posts, an array with the usernames and one with the embed codes
-     * 
+     *
      */
     public function show()
     {
       $posts = Post::all();
+      $nb_posts = count($posts);
       $username = DB::table('users')->join('posts', 'users.id', '=', 'posts.user_id')->select('users.name')->orderBy('posts.id')->get();
       $post_music_embed = DB::table('musics')->join('posts', 'musics.id', '=', 'posts.music_id')->select('musics.embed')->orderBy('posts.id')->get();
 
-      return view('home', ['posts' => $posts, 'username' => $username, 'embed' => $post_music_embed]);
+      return view('home', ['posts' => $posts, 'username' => $username, 'embed' => $post_music_embed, 'nb_posts' => $nb_posts]);
     }
 
     /**
