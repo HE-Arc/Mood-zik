4bb24aa3c1748689784bf2f555352674b51de99f
robin.alfred <robin.alfred@he-arc.ch>
2019-12-16 14:11:49 +0100

    Change home

--- Mood-zik/resources/views/home.blade.php
+++ Mood-zik/resources/views/home.blade.php
@@ -2,45 +2,25 @@
 
 
 @section('head')
-  <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/posts.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
-<section id="home">
+<section id="posts">
     <h1> Derniers Posts </h1>
-    <?php
-        for($i = $nb_posts-1; $i >=0; $i--) {
-            echo '<div class=post>';
-            echo '<div class="container_vertical">';
-            echo '<h2>' . $posts[$i]->title . '</h2>';
-            echo '<p class="author">';
-            echo 'Posté par <a href="'  . route('user', ['id' => $posts[$i]->user_id]) . '">' . $username[$i]->name . '</a>';
-            echo '</p>';
-            echo '<div class="container_horizental">';
-            echo '<p class="description">' . $posts[$i]->text . '</p>';
-            echo $embed[$i]->embed;
-            echo '<form action="' . route('add_to_playlist') . '" method="post">';
-            echo csrf_field();
-            echo '<input type="hidden" name="post_id" value="' . $posts[$i]->id . '" />';
-            echo '<input type="hidden" name="post_music_id" value="' . $posts[$i]->music_id . '" />';
-            echo '<input type="hidden" name="post_username" value="' . $username[$i]->name . '" />';
-            echo '<input type="submit" value="Add" />';
-            echo '</form>';
-            //echo '<br /><a href="{{ route(\'add_to_playlist\', [\'id\' => '. $posts[$i]->id . ']) }}">Ajouter ce post à ma playlist</a>';
-            echo '</div>';
-            echo '</div>';
-            echo '</div>';
 
-        }
+    @for ($i = $nb_posts-1; $i >= 0; $i--)
+      <div class=post>
+        <div class="container_vertical">
+          <h2>{{$posts[$i]->title}}</h2>
+          <p class="author">Posté par {{$username[$i]->name}}  </p>
+          <div class="container_horizental">
+            <p class="description"> {{$posts[$i]->text}} </p>
+            <?php echo $embed[$i]->embed; ?>
+          </div>
+        </div>
+      </div>
+    @endfor
 
-     ?>
-
-    <div class="card-body">
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
-    </div>
 </section>
 @endsection
